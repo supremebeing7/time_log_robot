fc1a42f000cf66e8247d48d1ae669feae3c3f25b
Mark J. Lehman <markopolo@gmail.com>
2016-05-19 20:23:38 -0700

    Add issue key parsing

---
+++ lib/time_log_robot/jira/issue_key_parser.rb
@@ -0,0 +1,29 @@
+module TimeLogRobot
+  module JIRA
+    class IssueKeyParser
+      class << self
+        def parse(entry)
+          matches = entry['description'].match(/(\[(?<issue_key>[^\]]*)\])/)
+          if matches.present?
+            matches['issue_key']
+          else
+            get_key_from_key_mapping(entry['description'])
+          end
+        end
+
+        private
+
+        def get_key_from_key_mapping(description)
+          mappings = YAML.load_file(mapping_file_path) || {}
+          if found_key = mappings.keys.find { |key| /#{description}/ =~ key }
+            mappings[found_key]
+          end
+        end
+
+        def mapping_file_path
+          File.join(TimeLogRobot.root, 'mapping.yml')
+        end
+      end
+    end
+  end
+end
