1570a2a5df275ed02a0967cd5076e109865459d6
Mark J. Lehman <markopolo@gmail.com>
2016-05-27 12:25:48 -0700

    Paginate toggl entries properly

--- lib/time_log_robot/toggl/report.rb
+++ lib/time_log_robot/toggl/report.rb
@@ -7,12 +7,14 @@ module TimeLogRobot
 
       base_uri 'https://toggl.com/reports/api/v2'
 
+      # TODO Refactor to avoid having to pass around `since` so much
       class << self
         def fetch(since: nil)
           since ||= Date.today.beginning_of_week(:saturday).to_time
           response = get('/details', basic_auth: auth, query: query(since))
           if response.success?
-            response['data']
+            pages = number_of_pages(response['total_count'])
+            aggregate_entries(response['data'], pages, since)
           else
             raise FetchError, response['error']
           end
@@ -20,6 +22,26 @@ module TimeLogRobot
         class FetchError < Exception; end
 
         private
+
+        def aggregate_entries(entries, pages, since)
+          2.upto(pages) do |page|
+            entries += get_entries_from_next_page(since, page)
+          end
+          entries
+        end
+
+        def get_entries_from_next_page(since, page)
+          built_query = query(since).merge(page: page)
+          response = get('/details', basic_auth: auth, query: built_query)
+          response['data']
+        end
+
+        def number_of_pages(count)
+          remaining_entries = count % 50
+          pages = count / 50
+          pages += 1 if remaining_entries > 0
+          return pages
+        end
 
         def auth
           {
